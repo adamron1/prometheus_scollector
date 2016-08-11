4a8cf138a650ded9e0f9cc7a2c5f83650ee2804f
Adam Ron <adam.ron@xero.com>
2016-08-11 14:58:47 +1200

    Ability to use as windows service

--- scollector_test.go
+++ scollector_test.go
@@ -14,70 +14,70 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"net/http"
-	"os"
-	"os/exec"
-	"strings"
-	"testing"
-	"time"
+  "bytes"
+  "io"
+  "net/http"
+  "os"
+  "os/exec"
+  "strings"
+  "testing"
+  "time"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/tgulacsi/go/loghlp/tsthlp"
+  "github.com/prometheus/client_golang/prometheus"
+  "github.com/tgulacsi/go/loghlp/tsthlp"
 )
 
 func TestScoll(t *testing.T) {
-	Log.SetHandler(tsthlp.TestHandler(t))
-	c := newScollectorCollector()
-	prometheus.MustRegister(c)
+  Log.SetHandler(tsthlp.TestHandler(t))
+  c := newScollectorCollector()
+  prometheus.MustRegister(c)
 
-	http.HandleFunc(*flagScollPref, c.handleScoll)
-	http.Handle("/metrics", prometheus.Handler())
-	Log.Info("Serving on " + *flagAddr)
-	go http.ListenAndServe(*flagAddr, nil)
+  http.HandleFunc(*flagScollPref, c.handleScoll)
+  http.Handle("/metrics", prometheus.Handler())
+  Log.Info("Serving on " + *flagAddr)
+  go http.ListenAndServe(*flagAddr, nil)
 
-	port := strings.SplitN(*flagAddr, ":", 2)[1]
-	cmd := exec.Command("scollector", "-h=http://localhost:"+port)
-	cmd.Stderr = os.Stderr
-	t.Logf("starting scollector")
-	if err := cmd.Start(); err != nil {
-		t.Skipf("Cannot start scollector: %v", err)
-		return
-	}
-	var body bytes.Buffer
-	for i := 0; i < 10; i++ {
-		if i > 0 {
-			time.Sleep(100 * time.Millisecond)
-		}
-		resp, err := http.Get("http://" + *flagAddr + "/metrics")
-		if err != nil {
-			t.Errorf("cannot get /metrics: %v", err)
-			t.FailNow()
-		}
-		body.Reset()
-		_, err = io.Copy(&body, resp.Body)
-		_ = resp.Body.Close()
-		if err != nil {
-			t.Logf("Response:\n%s", body.Bytes())
-			t.Errorf("Error reading response body: %v", err)
-			continue
-		}
-		if bytes.Contains(body.Bytes(), []byte("\nos_cpu{")) {
-			//t.Logf("Response:\n%s", body.Bytes())
-			break
-		}
-	}
-	_ = cmd.Process.Kill()
+  port := strings.SplitN(*flagAddr, ":", 2)[1]
+  cmd := exec.Command("scollector", "-h=http://localhost:"+port)
+  cmd.Stderr = os.Stderr
+  t.Logf("starting scollector")
+  if err := cmd.Start(); err != nil {
+    t.Skipf("Cannot start scollector: %v", err)
+    return
+  }
+  var body bytes.Buffer
+  for i := 0; i < 10; i++ {
+    if i > 0 {
+      time.Sleep(100 * time.Millisecond)
+    }
+    resp, err := http.Get("http://" + *flagAddr + "/metrics")
+    if err != nil {
+      t.Errorf("cannot get /metrics: %v", err)
+      t.FailNow()
+    }
+    body.Reset()
+    _, err = io.Copy(&body, resp.Body)
+    _ = resp.Body.Close()
+    if err != nil {
+      t.Logf("Response:\n%s", body.Bytes())
+      t.Errorf("Error reading response body: %v", err)
+      continue
+    }
+    if bytes.Contains(body.Bytes(), []byte("\nos_cpu{")) {
+      //t.Logf("Response:\n%s", body.Bytes())
+      break
+    }
+  }
+  _ = cmd.Process.Kill()
 
-	var lines []string
-	for _, line := range bytes.Split(body.Bytes(), []byte("\n")) {
-		if bytes.HasPrefix(line, []byte("os_disk_fs_percent_free{")) {
-			lines = append(lines, string(line))
-		}
-	}
-	if len(lines) < 2 {
-		t.Errorf("awaited at least 2 lines of \"os_disk_fs_percent_free\", got %d: %q",
-			len(lines), lines)
-	}
+  var lines []string
+  for _, line := range bytes.Split(body.Bytes(), []byte("\n")) {
+    if bytes.HasPrefix(line, []byte("os_disk_fs_percent_free{")) {
+      lines = append(lines, string(line))
+    }
+  }
+  if len(lines) < 2 {
+    t.Errorf("awaited at least 2 lines of \"os_disk_fs_percent_free\", got %d: %q",
+      len(lines), lines)
+  }
 }
